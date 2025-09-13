66d8d7ec15b2f4d7d64aeaed77c55862885fab6a
Mee1296 <weeraphat61100@gmail.com>
2025-09-13 14:53:22 +0700

    fix: fix sql command

--- SQL_command/user.sql
+++ SQL_command/user.sql
@@ -5,13 +5,13 @@ CREATE TABLE IF NOT EXISTS users (
     is_premium          BOOLEAN NOT NULL DEFAULT FALSE,
     user_state          user_state NOT NULL DEFAULT 'normal',
     user_name           TEXT UNIQUE,             -- unique username (ตั้งเองภายหลัง)
-    display_name        TEXT,
+    display_name        TEXT,                    -- non-unique display name
     education_level     TEXT,
     "like"              NUMERIC(10,4) DEFAULT 0,
     "dislike"           NUMERIC(10,4) DEFAULT 0,
     bio                 TEXT,
-    interested_subjects TEXT[],
-    profile_picture     TEXT,
+    interested_subjects TEXT[],                  -- array of subjects the user is interested in
+    profile_picture     TEXT,                    -- URL to profile picture
     created_at          TIMESTAMPTZ NOT NULL DEFAULT now(),
     updated_at          TIMESTAMPTZ NOT NULL DEFAULT now()
 );
