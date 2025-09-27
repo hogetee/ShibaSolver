3321b8f7c9588103f99d48850405ead16cdfea36
Mee1296 <weeraphat61100@gmail.com>
2025-09-27 20:56:21 +0700

    feat: add createpost

--- routers/postsRouter.js
+++ routers/postsRouter.js
@@ -1,11 +1,19 @@
 const express = require("express");
-const postsCtrl = require("../controllers/posts_controller");
+const postsCtrl = require("../controllers/postsController");
 const { requireAuth } = require("../middleware/auth");
 const router = express.Router();
 
+//GET
 router.get("/", postsCtrl.refreshFeed);
 router.get("/:id", postsCtrl.getPost);
-router.post("/bookmarks", requireAuth, postsCtrl.addBookmark);
 router.get("/bookmarks/:user_id", requireAuth, postsCtrl.getBookmarks);
+
+//POST
+router.post("/bookmarks", requireAuth, postsCtrl.addBookmark);
+
+//PUT
+router.put("/", postsCtrl.createPost);
+//DELETE
+
 // export router
 module.exports = router;
