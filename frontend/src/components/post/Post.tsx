35fe2d4d6415351b1f0e8be7c28c89aeb99fafc5
tangenn <wattanasukchaiparkorn@gmail.com>
2025-10-04 13:09:59 +0700

    Create dedicated post

--- frontend/src/components/post/Post.tsx
+++ frontend/src/components/post/Post.tsx
@@ -2,6 +2,8 @@ import PostHeader from './PostHeader';
 import PostContent from './PostContent';
 import PostAuthor from './PostAuthor';
 import TopComment from './TopComment';
+import { slugify } from '@/utils/slugify';
+import Link from 'next/link';
 
 export interface PostData {
   post_id: string;
@@ -39,27 +41,33 @@ interface PostProps {
   postData: PostData;
 }
 
+
 const Post = ({ postData }: PostProps) => {
+
+  const href = `/post/${postData.post_id}/${slugify(postData.title)}`
+
   return (
-    <div className="w-full min-h-[30vh] bg-white cursor-pointer hover:shadow-2xl/15 rounded-2xl shadow-lg p-6 flex flex-col font-display">
-      <div className="flex-grow">
-        <PostHeader isSolved={postData.is_solved} tags={postData.tags} />
-        <PostContent 
-          title={postData.title} 
-          description={postData.description}
-          postImage={postData.post_image} 
-        />
-        <PostAuthor author={postData.author} stats={postData.stats} />
+    <Link href={href} className="block">
+      <div className="w-full min-h-[30vh] bg-white cursor-pointer hover:shadow-2xl/15 rounded-2xl shadow-lg p-6 flex flex-col font-display">
+        <div className="flex-grow">
+          <PostHeader isSolved={postData.is_solved} tags={postData.tags} />
+          <PostContent 
+            title={postData.title} 
+            description={postData.description}
+            postImage={postData.post_image} 
+          />
+          <PostAuthor author={postData.author} stats={postData.stats} />
+        </div>
+        
+        <hr className="my-4 border-gray-200/80" />
+        
+        {postData.topComment ? (
+          <TopComment comment={postData.topComment} />
+        ) : (
+          <p className="text-center text-sm text-gray-400">No comments yet.</p>
+        )}
       </div>
-      
-      <hr className="my-4 border-gray-200/80" />
-      
-      {postData.topComment ? (
-        <TopComment comment={postData.topComment} />
-      ) : (
-        <p className="text-center text-sm text-gray-400">No comments yet.</p>
-      )}
-    </div>
+    </Link>
   );
 };
 
---
+++ frontend/src/components/post/dedicatedPost/DedicatedPostAuthor.tsx
@@ -0,0 +1,44 @@
+// Type สำหรับข้อมูลผู้เขียนและสถิติ
+type Author = {
+  display_name: string;
+  profile_picture: string;
+};
+type Stats = {
+  likes: number;
+  dislikes: number;
+};
+
+interface PostAuthorProps {
+  author: Author;
+  stats: Stats;
+}
+
+const PostAuthor = ({ author, stats }: PostAuthorProps) => {
+  return (
+    <div className="flex justify-between items-center">
+      <div className="flex items-center gap-3">
+        <img src={author.profile_picture} alt={`${author.display_name}'s avatar`} className="w-10 h-10 rounded-full" />
+        <span className="font-semibold text-dark-900">{author.display_name}</span>
+      </div>
+      <div className="flex items-center gap-4">
+        {/* Like Button */}
+        <div className="flex items-center gap-2 text-gray-600">
+          <button className="p-2 rounded-full hover:bg-black/5 active:bg-black/10 transition-colors">
+            <svg className="w-5 h-5 cursor-pointer text-gray-700" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M7 10v12" /><path d="M15 5.88 14 10h5.83a2 2 0 0 1 1.92 2.56l-2.33 8A2 2 0 0 1 17.5 22H4a2 2 0 0 1-2-2v-8a2 2 0 0 1 2-2h2.76a2 2 0 0 0 1.79-1.11L12 2h0a2 2 0 0 1 1.79 1.11L15 5.88Z" /></svg>
+          </button>
+          <span className="font-bold">{stats.likes}</span>
+        </div>
+        {/* Dislike Button */}
+        <div className="flex items-center gap-2 text-gray-600">
+          <button className="p-2 rounded-full  hover:bg-black/5 active:bg-black/10 transition-colors">
+            <svg className="w-5 h-5 cursor-pointer text-gray-700" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M17 14V2" /><path d="M9 18.12 10 14H4.17a2 2 0 0 1-1.92-2.56l2.33-8A2 2 0 0 1 6.5 2H20a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2h-2.76a2 2 0 0 0-1.79 1.11L12 22h0a2 2 0 0 1-1.79-1.11L9 18.12Z" /></svg>
+          </button>
+          <span className="font-bold">{stats.dislikes}</span>
+        </div>
+        
+      </div>
+    </div>
+  );
+};
+
+export default PostAuthor;
