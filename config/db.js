f96bf68ef7623e6e7543a8a5669ef007e3d8f3c7
Mee1296 <weeraphat61100@gmail.com>
2025-10-12 18:04:21 +0700

    deploy postgres to cloud

--- config/db.js
+++ config/db.js
@@ -2,21 +2,13 @@ const { Pool } = require('pg');
 
 const connectDB = async () => {
   try {
-    const pool = new Pool({
-      host: process.env.DB_HOST || 'localhost',
-      port: process.env.DB_PORT || 5432,
-      user: process.env.DB_USER || 'postgres',
-      password: process.env.DB_PASS || '',
-      database: process.env.DB_NAME || 'shibasolver',
-      max: 20,                 
-      idleTimeoutMillis: 30000 
-    });
-
     // test connection
-    const client = await pool.connect();
-    console.log(`PostgreSQL Connected: ${client.host}`);
-    client.release();
-
+    const pool = new Pool({
+    connectionString: 'postgresql://postgres.tqpfatrayjpkuqwhlkpx:ShibaSolver@aws-1-us-east-1.pooler.supabase.com:6543/postgres',
+    ssl: { rejectUnauthorized: false }
+    });
+    await pool.connect();
+    console.log('PostgreSQL connected successfully');
     return pool;
   } catch (err) {
     console.error('PostgreSQL connection error:', err.message);
