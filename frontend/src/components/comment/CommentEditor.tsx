b3811c0932723f48c4eb43ba4a01e70d7ad6ff74
Ittichet Thongsang <zagif2151382@gmail.com>
2025-10-13 20:20:43 +0700

    refactor: improve CommentEditor component structure and styling

--- frontend/src/components/comment/CommentEditor.tsx
+++ frontend/src/components/comment/CommentEditor.tsx
@@ -2,43 +2,44 @@ import type { CommentContent } from "./types";
 import { useState } from "react";
 
 interface CommentEditorProps {
-    initialContent: CommentContent;
-    onSave: (newContent: CommentContent) => void;
-    onCancel: () => void;
+  initialContent: CommentContent;
+  onSave: (newContent: CommentContent) => void;
+  onCancel: () => void;
 }
 
+export default function CommentEditor({
+  initialContent,
+  onSave,
+  onCancel,
+}: CommentEditorProps) {
+  const [content, setContent] = useState<CommentContent>(initialContent);
 
-export default function CommentEditor(
-    { initialContent, onSave, onCancel }: CommentEditorProps
-){
-    const [content, setContent] = useState<CommentContent>(initialContent);
+  const handleSubmit = () => {
+    onSave({ text: content.text });
+  };
 
-    const handleSubmit = () => {
-        onSave({text : content.text});
-    }
-
- return (
+  return (
     <div className="mt-2">
-        <textarea
+      <textarea
         value={content.text}
         onChange={(e) => setContent({ text: e.target.value })}
-        className="w-full p-2 border-2 border-gray-300 rounded-md focus:border-purple-500 outline-none"
+        className="w-full p-2 border-2 border-accent-200/75 rounded-md focus:border-accent-400 outline-none transition duration-300"
         rows={4}
-        />
-        <div className="flex justify-end mt-2 gap-2">
+      />
+      <div className="flex justify-end mt-2 gap-2">
         <button
-            onClick={onCancel}
-            className="px-4 py-2 bg-gray-300 text-gray-700 rounded-md hover:bg-gray-400"
+          onClick={onCancel}
+          className="px-4 py-2 bg-gray-200 hover:bg-gray-300 text-gray-700 rounded-md cursor-pointer transition duration-200"
         >
-            Cancel
+          Cancel
         </button>
         <button
-            onClick={handleSubmit}
-            className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700"
+          onClick={handleSubmit}
+          className="px-4 py-2 bg-accent-200/75 hover:bg-accent-200 text-gray-700 rounded-md cursor-pointer transition duration-200"
         >
-            Save
+          Save
         </button>
-        </div>
+      </div>
     </div>
- );
+  );
 }
