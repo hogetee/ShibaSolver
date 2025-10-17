bd0c89cf3044d4d4225580352a831d21d2ee1e25
Pornmongkolnano <pornmongkolnano@gmail.com>
2025-10-17 22:18:55 +0700

    fix bug

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { googleLogin, logout ,getMe} = require("../controllers/authController");
-const { requireAuth, optionalAuth } = require("../middleware/auth");
+const { requireAuth} = require("../middleware/auth");
 const router = express.Router();
 
 router.post("/google", googleLogin);
