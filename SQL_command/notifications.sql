1dbfcaeea0fd82ae00c699755173518382ef914a
Pornmongkolnano <pornmongkolnano@gmail.com>
2025-11-04 23:13:11 +0700

    implement enum to notification

--- SQL_command/notifications.sql
+++ SQL_command/notifications.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS notifications (
   notification_id SERIAL PRIMARY KEY,
   user_id INT NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
-  notification_type TEXT NOT NULL,   
+  notification_type notification_type NOT NULL,   
   message TEXT NOT NULL,
   link TEXT,
   is_read BOOLEAN DEFAULT FALSE,
