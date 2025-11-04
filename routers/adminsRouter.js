f67e5b147a7247bb7c981c2c18215141dd7fb7c6
ThanaponTh. <thanaponth.main@gmail.com>
2025-11-04 09:22:49 +0700

    change adminAction to true route

--- routers/adminsRouter.js
+++ routers/adminsRouter.js
@@ -12,6 +12,12 @@ router.delete('/posts/:postId', adminsCtrl.adminDeletePost);
 //PATCH
 router.patch('/users/:userId/ban', adminsCtrl.adminBanUser);
 router.patch('/users/:userId/unban', adminsCtrl.adminUnbanUser);
+router.patch("/accounts/:reportId/status", adminProtect, adminsCtrl.adminUpdateReportStatus);
+
+// Admin views account reports
+router.get("/accounts", adminProtect, adminsCtrl.adminGetAccountReports);
+router.get("/posts", adminProtect, adminsCtrl.adminGetPostReports);
+router.get("/comments", adminProtect, adminsCtrl.adminGetCommentReports);
 
 // export router
 module.exports = router;
--- routers/reportRouter.js
+++ routers/reportRouter.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const { requireAuth } = require("../middleware/auth");
-const { adminProtect } = require("../middleware/adminAuth");
 const reportCtrl = require("../controllers/reportController");
 
 const router = express.Router();
@@ -9,12 +8,5 @@ const router = express.Router();
 router.post("/accounts", requireAuth, reportCtrl.reportAccount);
 router.post("/content", requireAuth, reportCtrl.reportPostOrComment);
 
-// Admin views account reports
-router.get("/accounts", adminProtect, reportCtrl.adminGetAccountReports);
-router.get("/posts", adminProtect, reportCtrl.adminGetPostReports);
-router.get("/comments", adminProtect, reportCtrl.adminGetCommentReports);
-
-
-router.patch("/accounts/:reportId/status", adminProtect, reportCtrl.adminUpdateReportStatus);
 
 module.exports = router;
