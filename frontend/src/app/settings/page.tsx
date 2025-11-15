217c8ac4c73bebeeeae308fee2fb73d0f75917ac
noraaamor <pisitr93@gmail.com>
2025-11-15 15:34:05 +0700

    style: remove datapolicy link from setting page

--- frontend/src/app/settings/page.tsx
+++ frontend/src/app/settings/page.tsx
@@ -17,9 +17,9 @@ export default function SettingsPage() {
       <div className="w-full max-w-4xl min-h-screen flex flex-col gap-6 md:gap-8 items-stretch justify-center">
         <h1 className="text-5xl font-bold p-4 mb-2 text-primary-0">Settings</h1>
             <div className="flex flex-col gap-4">
-                <Link href="/data-policy" className="cursor-pointer w-full p-4 bg-accent-400 hover:bg-accent-600 text-white rounded-md text-center text-2xl font-medium transition-all duration-300">
+                {/* <Link href="/data-policy" className="cursor-pointer w-full p-4 bg-accent-400 hover:bg-accent-600 text-white rounded-md text-center text-2xl font-medium transition-all duration-300">
                     <p>View Data Policy</p>
-                </Link>
+                </Link> */}
                 <Link href="/admin" className="cursor-pointer w-full p-4 bg-accent-400 hover:bg-accent-600 text-white rounded-md text-center text-2xl font-medium transition-all duration-300">
                     <p>Admin Menu</p>
                 </Link>
